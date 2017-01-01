682adad56bdbe90f74ff5b28b9d51cff88b592c6
Almotasim Alhazmi <Almotasim.hassan@gmail.com>
2017-01-01 17:52:20 +0300

    Initial Commit

---
+++ pokadex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokadex
+//
+//  Created by Almotasim Alhazmi on 1/1/17.
+//  Copyright © 2017 Almotasim Alhazmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
